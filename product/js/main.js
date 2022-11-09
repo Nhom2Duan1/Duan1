a077a756bd1acb0aaad6dcd17a136d728cdbb29f
Ngoc Tuyen <tuyenlnph20246@fpt.edu.vn>
2022-11-09 21:21:55 +0700

    upload 90% form import goods

--- product/js/main.js
+++ product/js/main.js
@@ -13,3 +13,18 @@ for(let i = 0; i<=inputfocus.length;i++){
     inputfocus[i].style.border = "2px solid #2192FF";
   })
 }
+function seemoreclick(){
+  let seemore = document.querySelector('.seemore');
+  let textSeemore = document.querySelector('.text_seemore');
+  console.log(textSeemore);
+  if(seemore.style.display ===  'none'){
+    seemore.style.display =  'block';
+    textSeemore.textContent = 'Thu gọn'
+  } else{
+    seemore.style.display =  'none';
+    textSeemore.textContent = 'Xem thêm'
+  }
+  // if(seemore.style.display ==  'block'){
+  //   textSeemore.textContent = 'Thu gọn';seemore.style.display =  'none';
+  // }
+}
