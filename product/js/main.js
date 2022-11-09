501dc5ddfe1a954d2f4d931c7b2a45d567df0bb0
Ngoc Tuyen <tuyenlnph20246@fpt.edu.vn>
2022-11-09 19:44:09 +0700

    creat main js && add js form

--- product/js/main.js
+++ product/js/main.js
@@ -6,3 +6,10 @@ function checkpay(){
     document.querySelector(".payments").classList.remove('display');
   }
 }
+let inputfocus = document.querySelectorAll('.focus');
+console.log(inputfocus);
+for(let i = 0; i<=inputfocus.length;i++){
+  inputfocus[i].addEventListener("focus",()=>{
+    inputfocus[i].style.border = "2px solid #2192FF";
+  })
+}
