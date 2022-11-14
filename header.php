16deb8a16611874ed4955ada72f36ad6169b3d52
Giangchuate <105421404+Giangchuate@users.noreply.github.com>
2022-11-14 22:12:09 +0700

    giang

---
+++ header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>TPCG-FOOD</title>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+      integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <link rel="shortcut icon" href="img/logo-removebg.png" type="image/x-icon">
+    
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"
+    />
+    <link rel="stylesheet" href="home/hompage/style.css" />
+  </head>
+  <body>
+    <main class="container">
+      <div class="bg__header"></div>
+      <header id="scroll-header">
+        
+        <div class="header_container ">
+          <div class="btn__menu">
+            <i class="fa-solid fa-bars"></i>
+          </div>
+          <div class="header__logo">
+            <img src="img/logo-removebg.png" alt="" />
+          </div>
+          
+          <div class="header__menu">
+            
+            <ul class="list--menu close-menu ">
+              <div class="btn__close">
+                <i class="fa-solid fa-xmark"></i>
+              </div>
+              <li class="list--link"><a href="index.html">Trang chủ</a></li>
+              <li class="list--link"><a href="">Giới thiệu</a></li>
+              <li class="list--link"><a href="../book/book.html">Đặt bàn</a></li>
+              <li class="list--link"><a href="../menu/index.html">Thực đơn</a></li>
+              <li class="list--link"><a href="">Tin tức</a></li>
+              <li class="list--link"><a href="../contact/contact.html">Liên hệ</a></li>
+              <li class="list--link">
+                <a href="" class="hotline">Hotline:099999999</a>
+              </li>
+            </ul>
+          </div>
+          <div class="search">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <form action="" method="get" class="form__search">
+              <input type="text" name="" id="">
+              <button><i class="fa-solid fa-magnifying-glass"></i></button>
+            </form>
+          </div>
+        </div>
+      </header>
