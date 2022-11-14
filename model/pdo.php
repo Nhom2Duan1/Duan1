a9b72a3a8104a94754df8be3679568ee6475cc93
Giangchuate <105421404+Giangchuate@users.noreply.github.com>
2022-11-14 22:24:38 +0700

    giangupdate

--- model/pdo.php
+++ model/pdo.php
@@ -17,20 +17,20 @@ function pdo_get_connection(){
  * @param array $args mảng giá trị cung cấp cho các tham số của $sql
  * @throws PDOException lỗi thực thi câu lệnh
  */
-function pdo_execute($sql){
-    $sql_args = array_slice(func_get_args(), 1);
-    try{
-        $conn = pdo_get_connection();
-        $stmt = $conn->prepare($sql);
-        $stmt->execute($sql_args);
-    }
-    catch(PDOException $e){
-        throw $e;
-    }
-    finally{
-        unset($conn);
-    }
-}
+// function pdo_execute($sql){
+//     $sql_args = array_slice(func_get_args(), 1);
+//     try{
+//         $conn = pdo_get_connection();
+//         $stmt = $conn->prepare($sql);
+//         $stmt->execute($sql_args);
+//     }
+//     catch(PDOException $e){
+//         throw $e;
+//     }
+//     finally{
+//         unset($conn);
+//     }
+// }
 /**
  * Thực thi câu lệnh sql truy vấn dữ liệu (SELECT)
  * @param string $sql câu lệnh sql
