d96e9ccfebfa6b10a6040ffa86d044ad519f6a1d
Giangchuate <105421404+Giangchuate@users.noreply.github.com>
2022-11-16 22:31:02 +0700

    giang update

--- model/pdo.php
+++ model/pdo.php
@@ -3,7 +3,7 @@
  * Mở kết nối đến CSDL sử dụng PDO
  */
 function pdo_get_connection(){
-    $dburl = "mysql:host=localhost;dbname=tmart882;charset=utf8";
+    $dburl = "mysql:host=localhost;dbname=nhom2;charset=utf8";
     $username = 'root';
     $password = '';
 
@@ -17,20 +17,20 @@ function pdo_get_connection(){
  * @param array $args mảng giá trị cung cấp cho các tham số của $sql
  * @throws PDOException lỗi thực thi câu lệnh
  */
-// function pdo_execute($sql){
-//     $sql_args = array_slice(func_get_args(), 1);
-//     try{
-//         $conn = pdo_get_connection();
-//         $stmt = $conn->prepare($sql);
-//         $stmt->execute($sql_args);
-//     }
-//     catch(PDOException $e){
-//         throw $e;
-//     }
-//     finally{
-//         unset($conn);
-//     }
-// }
+function pdo_execute($sql){
+    $sql_args = array_slice(func_get_args(), 1);
+    try{
+        $conn = pdo_get_connection();
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($sql_args);
+    }
+    catch(PDOException $e){
+        throw $e;
+    }
+    finally{
+        unset($conn);
+    }
+}
 /**
  * Thực thi câu lệnh sql truy vấn dữ liệu (SELECT)
  * @param string $sql câu lệnh sql
---
+++ view/admin/header.php
@@ -0,0 +1,40 @@
+<div class="layout">
+                          <div class="layout__header">
+                                <h1 class="header--title">Tổng quan</h1>
+                                <div class="header--item">
+                                        <div>
+                                                <i class="fa-solid fa-wallet"></i>
+                                                <p>Vay vốn kinh doanh</p>
+                                        </div>
+                                        <div>
+                                                <i class="fa-solid fa-question"></i>
+                                                <p>Trợ giúp</p>   
+                                        </div>
+                                        <div>
+                                                <i class="fa-solid fa-heart"></i>
+                                                <p>Góp ý</p>
+                                        </div>
+                                        <div class="item-user">
+                                                <i class="fa-sharp fa-solid fa-circle-user"></i>
+                                                <p>User Name</p>
+                                                <i class="fa-solid fa-chevron-down"></i>
+                                        </div>
+                                        <div>
+                                                <i class="fa-solid fa-bell"></i>
+                                                
+                                        </div>
+                                </div>
+                                <div class="header__list-user">
+                                   <div class="list-user">
+                                        <i class="fa-sharp fa-solid fa-circle-user"></i>
+                                        <p>Tài khoản của tôi</p>
+                                   </div>
+                                   <div class="list-user">
+                                        <i class="fa-solid fa-right-from-bracket"></i>
+                                        <p>Đăng xuất tài khoản</p>
+                                   </div>
+                                   
+                                   
+                                </div>
+                          </div>
+                     
