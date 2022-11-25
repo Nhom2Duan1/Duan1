32095c427db18cc43560c552058e9c5cdd063b06
Ngoc Tuyen <tuyenlnph20246@fpt.edu.vn>
2022-11-25 12:27:51 +0700

    souce code model/menu

---
+++ model/menu/menu.php
@@ -0,0 +1,43 @@
+<?php
+//Thêm món ăn
+function insert_addmenu($anh_mon,$ten_mon,$gia_mon,$danh_gia_mon,$menu_list){
+  $sql = "insert into mon_an(id_mon,anh_mon,ten_mon,gia_mon,danh_gia_mon,menu_list) values(NULL,'$anh_mon','$ten_mon','$gia_mon','$danh_gia_mon','$menu_list')";
+  pdo_execute($sql);
+}
+//load all món ăn
+function loadAll_menu(){
+  $sql = "select * from mon_an";
+  $all_menu = pdo_query($sql);
+  return $all_menu;
+  }
+//Xóa món ăn
+function delete_menu($id_mon){
+  $sql = "delete from mon_an where id_mon=".$id_mon;
+  pdo_execute($sql);
+}
+//Load one món ăn
+function fix_menu($id_mon){
+  $sql = "select * from mon_an where id_mon=".$id_mon;
+  $fixmenu = pdo_query_one($sql);
+  return $fixmenu;
+}
+//CẬP NHẬT MÓN ĂN
+function update_menu($anh_mon,$ten_mon, $gia_mon,$danh_gia_mon,$menu_list,$id_mon){
+  if($anh_mon!= ""){
+    $sql = "update mon_an set anh_mon='".$anh_mon."',ten_mon = '".$ten_mon."',gia_mon = '".$gia_mon."',danh_gia_mon = '".$danh_gia_mon."',menu_list = '".$menu_list."'where id_mon=".$id_mon;
+  }
+  else{
+    $sql = "update mon_an set ten_mon = '".$ten_mon."',gia_mon = '".$gia_mon."',danh_gia_mon = '".$danh_gia_mon."',menu_list = '".$menu_list."'where id_mon=".$id_mon;
+  }
+  pdo_execute($sql);
+}
+//LOAD MÓN ĂN THEO DANH MỤC
+function loadAll_liscate_menu($menu_list=0){
+  $sql = "select * from mon_an where 1"; 
+  if($menu_list>0){
+      $sql.=" and menu_list='".$menu_list."'";
+  }
+  $listmenu_cate = pdo_query($sql);
+  return $listmenu_cate;
+}
+?>
