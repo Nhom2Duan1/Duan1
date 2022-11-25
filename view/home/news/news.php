50f3ccb093610211776f9b695df170fd853a1a0c
Giangchuate <105421404+Giangchuate@users.noreply.github.com>
2022-11-25 12:21:23 +0700

    giang view home new

--- view/home/news/news.php
+++ view/home/news/news.php
@@ -1,136 +1,55 @@
-<!DOCTYPE html>
-<html lang="en">
+<div class="content" >
+     <div class="post" style="height:fit-content">
+            <div class="post_content-left" >
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Tin tức</title>
-  <link rel="stylesheet" href="style.css">
-  <link rel="stylesheet" href="../hompage/style.css">
-  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+           
+                      <?php
+                        foreach ($news as $listnews) {
+                          extract($listnews);
+                          $linknews="?ctr=newsct&&id_tintuc=".$id_tintuc;
+                          echo ' <div class="post_left">
+                          <div class="item">
+                            <div class="date">'.$date.'</div>
+                            <a href="'.$linknews.'"> <img src="./view/home/news/img/' . $image . '" alt="" style="width:400px"></a>
+                          </div>
+                          <div class="letter">
+                            <a href="'.$linknews.'">
+                              <h1>' . $title . '</h1>
+                              <span>' .$date. '</span>
+                            </a>
+                            <hr width="30px">
+                            <a href="'.$linknews.'">
+                              <p>' . $summary . '</p>
+                            </a>
+                          </div>
+                        </div>';
+                        }
 
-</head>
 
-<body>
-  <div class="container">
-    
-    <div class="post">
-      <div class="content">
-        <div class="post_left">
-        <div class="item">
-          <div class="date">06 <br> Th2</div>
-          <a href=""> <img src="img/img5.jpg" alt="" width="100%"></a>
-        </div>
-        <div class="letter">
-          <a href="">
-            <h1>“ĂN NGON – QUÀ GIÒN” với voucher tặng món tại TPCG-Food</h1>
-          </a>
-          <hr width="30px">
-          <a href="">
-            <p>Nhằm tri ân khách hàng thân mến đã đồng hành cùng chúng tôi trong [...]</p>
-          </a>
-        </div>
-      </div>
-      <div class="post_left">
-        <div class="item">
-          <div class="date">06 <br> Th2</div>
-          <a href=""> <img src="img/img4.png" alt="" width="100%"></a>
-        </div>
-        <div class="letter">
-          <a href="">
-            <h1>Khai trương TPCG-Food với nhiều ưu đãi hấp dẫn</h1>
-          </a>
-          <hr width="30px">
-          <a href="">
-            <p>Ngày 26/4 vừa qua, vua nướng Hàn Quốc sẽ tiếp tục chuyến[...]</p>
-          </a>
-        </div>
-      </div>
-      <div class="post_left">
-        <div class="item">
-          <div class="date">06 <br> Th2</div>
-          <a href=""> <img src="img/img3.jpg" alt="" width="100%"></a>
-        </div>
-        <div class="letter">
-          <a href="">
-            <h1>TPCG-Food từng bừng khai trương mừng năm mới 2020</h1>
-          </a>
-          <hr width="30px">
-          <a href="">
-            <p>Công ty cổ phần đầu tư thương mại Quốc tế Mặt Trời Đỏ (REDSUN ITI) [...]</p>
-          </a>
-        </div>
-      </div>
-      <div class="post_left">
-        <div class="item">
-          <div class="date">06 <br> Th2</div>
-          <a href=""> <img src="img/img5.jpg" alt="" width="100%"></a>
-        </div>
-        <div class="letter">
-          <a href="">
-            <h1>Bạn đã sẵn sàng nhập tiệc cùng TPCG-Food dịp cuối năm chưa?</h1>
-          </a>
-          <hr width="30px">
-          <a href="">
-            <p>Tất nhiên năm nay không còn đau đầu suy nghĩ tìm kiếm địa điểm [...]</p>
-          </a>
-        </div>
-      </div>
-      <div class="post_left">
-        <div class="item">
-          <div class="date">06 <br> Th2</div>
-          <a href=""> <img src="img/img3.jpg" alt="" width="100%"></a>
-        </div>
-        <div class="letter">
-          <a href="">
-            <h1>TPCG-Food chào tất cả mọi người!</h1>
-          </a>
-          <hr width="30px">
-          <a href="">
-            <p>Nhằm tri ân khách hàng thân mến đã đồng hành cùng chúng tôi trong [...]</p>
-          </a>
-        </div>
-      </div>
-     </div>
-      
+                        ?>
+   </div>
 
-      <div class="post_right">
-        <input type="text" placeholder="Search..."> <button><i class='bx bx-search'></i></button>
-        <div class="title">
-          <h1>BÀI VIẾT MỚI</h1>
-        <hr>
-        </div>
-        <div class="post_details">
-          <a href="">“ĂN NGON – QUÀ GIÒN” với voucher tặng món tại TPCG-Food</a>
-          <hr>
-        </div>
-        <div class="post_details">
-          <a href="">Khai trương TPCG-Food với nhiều ưu đãi hấp dẫn</a>
-          <hr>
-        </div>
-        <div class="post_details">
-          <a href="">TPCG-Food tưng bừng khai trương mừng năm mới 2018</a>
-          <hr>
-        </div>
-        <div class="post_details">
-          <a href="">Bạn đã sẵn sàng nhập tiệc cùng TPCG-Food dịp cuối năm chưa?</a>
-          <hr>
-        </div>
-        <div class="post_details">
-          <a href="">TPCG-Food chào tất cả mọi người!</a>
-          <hr>
-        </div>
-        <div class="title">
-          <h1>CHUYÊN MỤC</h1>
-        <hr>
-        </div>
-        
-      </div>
+
+                      <div class="post_right">
+                        <div class="title">
+                          <h1>BÀI VIẾT MỚI</h1>
+                          <hr>
+                        </div>
+                        <?php
+                            foreach ($news as $titlenews) {
+                              extract($titlenews);
+                              echo '<div class="post_details">
+                              <a href="'.$linknews.'">' . $title . '</a>
+                              <hr>
+                            </div>';
+                            }
+
+                            ?>
+                        <div class="title">
+                          <h1>CHUYÊN MỤC</h1>
+                          <hr>
+                        </div>
+                      </div>
+                      <!-- end post_right -->
     </div>
-   
-  </div>
-  
-</body>
-
-</html>
+</div>
