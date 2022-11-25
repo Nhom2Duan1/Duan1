1bfe958e1a71c6b2ac83715669d58f4e020c41d0
Giangchuate <105421404+Giangchuate@users.noreply.github.com>
2022-11-25 12:19:38 +0700

    giang view homepage

--- view/home/hompage/header.php
+++ view/home/hompage/header.php
@@ -9,10 +9,16 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
     integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="shortcut icon" href="./hompage/img/logo-removebg.png" type="image/x-icon">
+  <link rel="shortcut icon" href="./view/home/hompage/img/logo-removebg.png" type="image/x-icon">
 
   <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+  <link rel="stylesheet" href="./view/home/news/style.css" />
+   <link rel="stylesheet" href="./view/home/news/style1.css" />
+   <link rel="stylesheet" href="./view/home/introduce/style.css" />
+  <link rel="stylesheet" href="./view/home/acount/signup/style.css" />
   <link rel="stylesheet" href="./view/home/hompage/style.css" />
+ 
+  
 </head>
 
 <body>
@@ -35,10 +41,10 @@
               <i class="fa-solid fa-xmark"></i>
             </div>
             <li class="list--link"><a href="?ctr=homepage">Trang chủ</a></li>
-            <li class="list--link"><a href="">Giới thiệu</a></li>
+            <li class="list--link"><a href="?ctr=introduce">Giới thiệu</a></li>
             <li class="list--link"><a href="?ctr=settable">Đặt bàn</a></li>
             <li class="list--link"><a href="?ctr=menu">Thực đơn</a></li>
-            <li class="list--link"><a href="">Tin tức</a></li>
+            <li class="list--link"><a href="?ctr=news">Tin tức</a></li>
             <li class="list--link"><a href="?ctr=contact">Liên hệ</a></li>
             <li class="list--link"><a href="?ctr=signin">Đăng nhập</a></li>
             <li class="list--link">
