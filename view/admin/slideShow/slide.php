af7682bf3588ff54e07a86f3ef568833fe6c6d0a
Giangchuate <105421404+Giangchuate@users.noreply.github.com>
2022-11-25 12:16:37 +0700

    giang view slide

--- view/admin/slideShow/slide.php
+++ view/admin/slideShow/slide.php
@@ -1,19 +1,42 @@
 
 <div class="content">
-<h1 style="color:black"> Update slide </h1>
-<h1> Update slide </h1>
-<h1> Update slide </h1>
+        <div class="slideShow__container">
+                <h1 class="slideShow__title">Danh sách banner</h1> 
+                <div class="slideShow__table-container">
+                <table class="table">
+                        <thead>
+                        <tr class="table--header">
+                                <th>ID</th>
+                                <th>IMAGE</th>
+                                <th ><i class="fa-solid fa-pen-to-square icon"></i></th>
+                                
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                        foreach ($listslider as $slider) {
+                         extract($slider);
+                         $update_slider="?ctr=upateslide&id_slidershow=".$id_slidershow;
+                         echo '
+                         <tr class="table--body">
+                                <td>'.$id_slidershow.'</td>
+                                <td class="img"><img src="../../view/home/hompage/img/'.$image.'" alt=""></td>
+                                <td ><a href="'.$update_slider.'" ><i class="fa-solid fa-pen-to-square icon"></i></a></td>
+                                
+                        </tr>';
+                        }
 
-<h1> Update slide </h1>
-<h1> Update slide </h1>
-viewfa-flip-verticalv
+
+                        ?>
+                        </tbody>
+                </table>
+                </div>
+                
+        </div>
+              
 
 
-<h1> Update slide </h1>
-v
-<h1> Update slide </h1>
-v
-<h1> Update slide </h1>                   
 
 </div>
+<!-- end -->
 </div>
