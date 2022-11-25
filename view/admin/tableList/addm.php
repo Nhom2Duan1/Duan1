24889e919646cc11e92b4cd112e20fab7b49b966
nguyenchuen2003 <106584958+nguyenchuen2003@users.noreply.github.com>
2022-11-25 12:23:02 +0700

    view add  danh muc

---
+++ view/admin/tableList/addm.php
@@ -0,0 +1,14 @@
+
+   <div class="add_dm">
+         <form action="" method="post">
+          <div>
+          <input required name="ten_danh_muc" class="tt" type="text" placeholder="Tên Danh Mục"> <br> <br>
+          <input required name="so_luong" class="tt" type="text" placeholder="Số Lượng Danh Mục">
+
+          </div>
+          <div class="submit">
+        <a href="?ctr=tableList"><input  name="btn" type="submit" value="Thêm Mới"></a>  
+          </div>
+         </form>
+        
+   </div>
