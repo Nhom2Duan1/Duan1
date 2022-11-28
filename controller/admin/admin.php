f7143287b3e17b7a6eb01cd641ea1a0e1f2745b8
nguyenchuen2003 <106584958+nguyenchuen2003@users.noreply.github.com>
2022-11-29 00:24:21 +0700

    nguyenchien_controller

--- controller/admin/admin.php
+++ controller/admin/admin.php
@@ -1,6 +1,10 @@
 <?php
 include_once '../../view/admin/nav.php';
 include_once '../../view/admin/header.php';
+include_once '../../model/contact/contact.php';
+include_once '../../model/tablelist/tablelist.php';
+
+include_once '../../model/pdo.php';
 if(isset($_GET['ctr'])){
   $ctr = $_GET['ctr'];
   switch ($ctr) {
@@ -11,7 +15,32 @@ if(isset($_GET['ctr'])){
     case 'slide':
       include_once '../../view/admin/slideShow/slide.php';
       break;
+    case 'contact':
+      $list_lh= listall_lh();
+      include_once '../../view/admin/contact/contact.php';
+      break;
+    case 'tableList':
+      $list_dm =  listall_dm() ;
+        include_once '../../view/admin/tablelist/tablelist.php';
+        break;
+    case 'xoadm':
+         if(isset($_GET['id_loai_ban'])&&($_GET['id_loai_ban'])>0){
+          $sql = "delete from loai_ban where id_loai_ban =".$_GET['id_loai_ban'];
+          pdo_execute($sql);
+         }
+        $list_dm =  listall_dm() ;
+        include_once '../../view/admin/tablelist/tablelist.php';
+        break;
+        case 'adddm':
+          if(isset($_POST['btn'])&&($_POST['btn'])){
+            $ten_danh_muc = $_POST['ten_danh_muc'];
+            $so_luong = $_POST['so_luong'];
+            insert_dm( $ten_danh_muc ,$so_luong);
 
+          }
+          $list_dm =  listall_dm() ;
+          include_once '../../view/admin/tablelist/addm.php';
+          break;
     default:
       include_once '../../view/admin/home.php';
       break;
--- controller/clien/home.php
+++ controller/clien/home.php
@@ -1,5 +1,10 @@
 <?php
 include_once './view/home/hompage/header.php';
+include_once 'model/book/book.php';
+include_once 'model/contact/contact.php';
+
+include_once 'model/tablelist/tablelist.php';
+include_once 'model/pdo.php';
 if(isset($_GET['ctr'])){
   $ctr = $_GET['ctr'];
   switch ($ctr) {
@@ -16,10 +21,35 @@ if(isset($_GET['ctr'])){
       break;
       // Liên hệ
     case 'contact':
+         if(isset($_POST['btn'])&&($_POST['btn'])){
+          $ten_khach = $_POST['name'];
+          $email = $_POST['email'];
+          $sdt = $_POST['sdt'];
+          $muc_dich = $_POST['mucdich'];
+          $noi_dung = $_POST['noidung'];
+          insert_lienhe($ten_khach, $email, $sdt, $muc_dich, $noi_dung) ;
+           $thongbao = "bạn đã gửi yêu cầu liên hệ thành công" ;
+         }
       include_once './view/home/contact/contact.php';
       break;
       // Đặt bàn
     case 'settable':
+      if(isset($_POST['btn_bt'])&&($_POST['btn_bt'])){
+        $ten_nguoi_dat = $_POST['ten_nguoi_dat'];
+        $sdt = $_POST['sdt'];
+        $so_nguoi = $_POST['so_nguoi'];
+        $ngay= $_POST['ngay'];
+
+        $gio = $_POST['gio'];
+        $gio1 = implode(",",$gio);
+        $id_loai_ban = $_POST['id_loai_ban'];
+       
+        insert_dat_ban($ten_nguoi_dat,$sdt,$so_nguoi, $ngay,$gio1,$id_loai_ban);
+       
+      
+      }
+      $listdm = listall_dm();
+    
       include_once './view/home/book/book.php';
       break;
       // Đăng nhập
