aaaebec56290a911e7f40bfea4b4bd8ff264ebb4
phuong1901203 <vietphuong19012003@gmail.com>
2022-11-29 23:30:37 +0700

    header

--- view/home/hompage/header.php
+++ view/home/hompage/header.php
@@ -13,6 +13,15 @@
 
   <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
   <link rel="stylesheet" href="./view/home/hompage/style.css" />
+  <link rel="stylesheet" href="./view/home/acount/signin/style.css" />
+  <link rel="stylesheet" href="./view/home/acount/signup/style.css" />
+  <link rel="stylesheet" href="./view/home/hompage/style.css" />
+  <link rel="stylesheet" href="./view/home/contact/contact.css">
+  <link rel="stylesheet" href="./view/home/book/book.css">
+  <link rel="stylesheet" href="./view/home/news/style.css" />
+  <link rel="stylesheet" href="./view/home/news/style1.css" />
+  <link rel="stylesheet" href="./view/home/introduce/style.css" />
+  <link rel="stylesheet" href="./view/home/acount/signup/style.css" />
 </head>
 
 <body>
@@ -27,7 +36,7 @@
         <div class="header__logo">
           <img src="./view/home/hompage/img/logo-removebg.png" alt="" />
         </div>
-
+ 
         <div class="header__menu">
 
           <ul class="list--menu close-menu ">
@@ -35,15 +44,45 @@
               <i class="fa-solid fa-xmark"></i>
             </div>
             <li class="list--link"><a href="?ctr=homepage">Trang chủ</a></li>
-            <li class="list--link"><a href="">Giới thiệu</a></li>
+            <li class="list--link"><a href="?ctr=introduce">Giới thiệu</a></li>
             <li class="list--link"><a href="?ctr=settable">Đặt bàn</a></li>
             <li class="list--link"><a href="?ctr=menu">Thực đơn</a></li>
-            <li class="list--link"><a href="">Tin tức</a></li>
+            <li class="list--link"><a href="?ctr=news">Tin tức</a></li>
             <li class="list--link"><a href="?ctr=contact">Liên hệ</a></li>
+            <li class="list--link"><a href="#" class="hotline">Hotline:099999999</a></li>
+           <?php 
+            if(isset($_SESSION['ten_tk'])){                  
+           ?> 
+            <li class="list--link"> <a href="?ctr=log_out" style="color: #AAAAAA;">Đăng Xuất</a> </li>
+           <?php } else{
+
+           ?>
             <li class="list--link"><a href="?ctr=signin">Đăng nhập</a></li>
-            <li class="list--link">
-              <a href="controller/admin/admin.php" class="hotline">Hotline:099999999</a>
+           <?php  }?>
+            
+            <li class="list--link"><a href="">
+              <?php
+              if(isset($_SESSION['ten_tk'])){
+                ?>
+                <span> Hello </span> <?php echo $_SESSION['ten_tk'] ['ten_tk'] ?>
+                 
+               <?php }?>
+            </a>
+          </li> 
+          <?php
+       if (isset($_SESSION['ten_tk'])) { 
+           extract($_SESSION['ten_tk']);
+         ?>
+    
+            <?php if($vai_tro == 1){ ?>
+            <li class="list--link" >
+              <a href="controller/admin/admin.php" style="color:#AAAAAA;"  >Đăng nhập admin</a>
             </li>
+            <?php } ?>
+         
+         <?php
+       } 
+     ?>
           </ul>
         </div>
         <div class="search">
